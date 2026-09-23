84d3c14286492bed0528ce33e8b5d052829e971d
Danita Sharon <danitasharon@Danitas-MacBook-Pro.local>
2026-09-23 15:30:50 +0000

    Add e-commerce tasks app

---
+++ tasks-app/db.php
@@ -0,0 +1,9 @@
+<?php
+$host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "tasks_app";
+$conn = new mysqli($host, $db_user, $db_pass, $db_name);
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
